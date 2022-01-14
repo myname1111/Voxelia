8a137facce2c3d2f54e81b9f35ffd9efe0d68d4a
myname1111 <animationstudiocp@gmail.com>
2022-01-14 11:07:55 +0700

    pong

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from kivy.graphics.vertex_instructions import Rectangle, Ellipse
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import StringProperty, Clock
 from kivy.uix.widget import Widget
+import os
+import keyboard
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
 
 class Menu(GridLayout):
@@ -22,29 +25,21 @@ class MainWidget(Widget):
         super().__init__(**kwargs)
         self.rebound = ["Right", "Up"]
         with self.canvas:
-            Color(0, .9, 0)
             self.ball = Ellipse(pos=(200, 100), size=(100, 100))
+            self.player1 = Rectangle(pos=(700, 100), size=(20, 150))
+            self.player2 = Rectangle(pos=(100, 100), size=(20, 150))
+            self.ball_speed = [5, 5]
         Clock.schedule_interval(self.update, 1/60)
 
     def update(self, dt):
         x, y = self.ball.pos
-        speed = (2, 2)
-        if x >= self.width - self.ball.size[0]:
-            self.rebound[0] = "Left"
-        elif x <= 0:
-            self.rebound[0] = "Right"
-        if y >= self.height - self.ball.size[1]:
-            self.rebound[1] = "Down"
-        elif y <= 0:
-            self.rebound[1] = "Up"
-        if self.rebound[0] == "Right":
-            x += speed[0]
-        elif self.rebound[0] == "Left":
-            x -= speed[0]
-        if self.rebound[1] == "Up":
-            y += speed[1]
-        elif self.rebound[1] == "Down":
-            y -= speed[1]
+        print(keyboard.is_pressed('up arrow'))
+        x += self.ball_speed[0]
+        y += self.ball_speed[1]
+        if y > self.height - self.ball.size[1]:
+            self.ball_speed[1] = -abs(self.ball_speed[1])
+        if y < 0:
+            self.ball_speed[1] = abs(self.ball_speed[1])
         self.ball.pos = (x, y)
 
 
